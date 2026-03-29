018bc4b665189eb482dff16759939f9c003e50d7
HaoMEI423 <s4164444@student.rmit.edu.au>
2026-03-30 06:37:10 +1100

    add draw lines

--- mappingMain.js
+++ mappingMain.js
@@ -1,5 +1,5 @@
 // find our elements 
-const stageContainer = document.getElementById("stag-container");
+const stageContainer = document.getElementById("stage-container");
 console.log(stageContainer);
 const circleButton = document.getElementById("circle-button");
 
@@ -40,3 +40,63 @@ function drawNewCircle(){
 }
 
 circleButton.addEventListener("click",drawNewCircle);
+
+
+// drawing feature
+// feature analysis
+// what is the user goal? trying to draw a picture
+// what is the represented model? cursor on the canvas : defined canvas : brush select : brush on?
+// colour? or would that be its own system?
+// how does it behaves?
+// move our cursor onto canvas, press mouse button down, move mouse, release mouse button
+// what is the implemented model? create a new line when mouse button down, add to that line when mouse moves
+// how does it interact with other features?
+// colour, images for the brush, eraser, uploaded image, 
+
+// keep track of when button is held
+let isDrawing = false;
+let lastLine; 
+
+// user presses mouse button
+function drawMouseDown(){
+    isDrawing= true;
+    const pos =stage.getPointerPosition();
+    lastLine = new Konva.Line({
+        stroke: "red",
+        strokeWidth: 5,
+        lineCap: "round",
+        lineJoin: "round",
+        points: [pos.x, pos.y, pos.x, pos.y]
+    });
+    firstLayer.add(lastLine);
+}
+// add function to mousedown event
+stage.on("mousedown", drawMouseDown);
+
+// user moves their mouse
+function drawMouseMove(){
+    console.log(Date.now())
+    // don't run if not drawing
+    if(isDrawing === false){
+        return;
+    }
+    // if isDrawing is true
+    const pos = stage.getPointerPosition();
+    let newPoints = lastLine.points().concat([pos.x, pos.y]);
+    lastLine.points(newPoints);
+}
+// add function to mouse move event
+stage.on("mousemove", drawMouseMove);
+
+// user releases mouse button
+function drawMouseUp(){
+    isDrawing= false;
+}
+// add function to mouse up event
+//stage.on("mouseup", drawMouseUp);
+window.addEventListener("mouseup", drawMouseUp);
+
+
+// user goal? to click buttons on the web
+// what is the represented model? cursor on canvas, cursor down, cursor up, button feedback
+// how does it behave? move cursor around web page, press mouse down, move mouse, release mouse buttopn
